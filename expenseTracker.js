9439650cd6360043bf7028db89930f92a3335c2d
Akash Bansode <akash.b@prometteur.in>
2024-07-11 11:13:24 +0530

    Added html for expense tracker

---
+++ expenseTracker.js
@@ -0,0 +1,23 @@
+const expenseList = [
+  { expense: "Test", expenseType: "income", amount: 100 },
+  { expense: "Test", expenseType: "income", amount: 200 },
+];
+
+// renderExpenseList()
+
+function renderExpenseList() {
+  let expenseListHTML = "";
+
+  expenseList.forEach();
+}
+
+function addExpense() {
+  const expenseDescriptionElement = document.querySelector(".js-expense-input");
+  const expenseAmountElement = document.querySelector(
+    ".js-expense-amount-input"
+  );
+  const selectElement = document.querySelector("#select1");
+
+  const description = expenseDescriptionElement.ariaValueMax;
+  
+}
